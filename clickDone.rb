c8d1ac50fd22aa3486865bcbf1a77577e125b9ce
kgarber <kgarber@zerionsoftware.com>
2016-02-05 11:26:38 -0600

    Update clickDone.rb
    TESTINGAPI-14 #start-progress

--- clickDone.rb
+++ clickDone.rb
@@ -6,7 +6,7 @@ end
 @driver.find_element(:uiautomator, "new UiSelector().className(\"android.widget.Button\")").click()
 @driver.find_element(:uiautomator, "new UiSelector().resourceId(\"android:id/action_bar_title\")").click()
 end
-#That was just jibberish2333333123213333
+#That was just jibberish23333331232133332323
 def syncDevice()
 	@driver.find_element(:uiautomator, "new UiSelector().resourceId(\"com.zerion.apps.iform.appium:id/activity_form_list_action_sync\")").click()
 @driver.manage.timeouts.implicit_wait = 60
